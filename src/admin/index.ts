9142b508a14a98a915407b5c67ad979f19e1c14b
Erick Soares do Couto <ecouto123@gmail.com>
2025-10-07 20:49:25 -0300

    fix error

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,10 +1,13 @@
-import BlingSettingsPage, {
-  config as blingSettingsConfig,
-} from "./settings/bling-settings"
+import BlingSettingsPage, { config as blingSettingsConfig } from "./settings/bling-settings"
 
 export const widgets: Array<() => JSX.Element> = []
 export const routes: never[] = []
-export const settings: never[] = []
+export const settings = [
+  {
+    Component: BlingSettingsPage,
+    config: { ...blingSettingsConfig },
+  },
+]
 
 const entry = {
   identifier: "medusa-plugin-bling",
