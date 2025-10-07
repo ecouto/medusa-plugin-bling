8d24a68b834bc7e650eae04f8711068341f3953b
Erick Soares do Couto <ecouto123@gmail.com>
2025-10-07 16:32:28 -0300

    Ajuste versao

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,5 +1,11 @@
 import BlingSettingsWidget from "./widgets/bling-settings-widget";
 
 export const widgets = [
-    BlingSettingsWidget
+    BlingSettingsWidget,
 ];
+
+const adminIntegration = {
+    widgets,
+};
+
+export default adminIntegration;
